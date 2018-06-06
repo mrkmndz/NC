2f19e3691bc1c27a5cfb179e88239408af99706a
Mark Mendoza <mendoza.mark.a@gmail.com>
2018-06-06 22:01:14 +0000

    use that data

--- server/server.py
+++ server/server.py
@@ -7,46 +7,14 @@ import sys
 sys.path.append('../include')
 from constants import *
 from headers import *
+from db import kv
 
 path_kv = "kv.txt"
 path_log = "server_log.txt"
 
-len_key = 16
-len_val = 128
-
-f = open(path_kv, "r")
-lines = f.readlines()
-f.close()
-
-kv = {}
-for i in range(2, 3002, 3):
-    line = lines[i].split();
-    key_header = line[0]
-    key_body = line[1:]
-    val = lines[i + 1].split()
-    
-    key_header = int(key_header)
-    for i in range(len(key_body)):
-        key_body[i] = int(key_body[i], 16)
-    for i in range(len(val)):
-        val[i] = int(val[i], 16)
-    
-    key_field = ""
-    key_field += struct.pack(">I", key_header)
-    for i in range(len(key_body)):
-        key_field += struct.pack("B", key_body[i])
-    
-    val_field = ""
-    for i in range(len(val)):
-        val_field += struct.pack("B", val[i])
-    
-    kv[key_header] = (key_field, val_field)
-f.close()
-
 def lookup_val(key):
     key_header = struct.unpack(">I", key[:4])[0]
-    key_field, val_field = kv[key_header]
-    return val_field
+    return kv[key_header]
 
 counter = 0
 def counting():
