2a5346ca1e6ab6a30be743ad5158aec80a0c0e67
Mark Mendoza <mendoza.mark.a@gmail.com>
2018-06-06 22:02:46 +0000

    ugly hack

--- include/db.py
+++ include/db.py
@@ -2,7 +2,8 @@ from constants import *
 
 NUM_KEYS = 1000
 kv = {}
-with open("secret-cache.txt", "r") as f:
+# TODO use __FILE__
+with open("../include/secret-cache.txt", "r") as f:
     f.read(1000)
     for x in range(NUM_KEYS):
         kv[x + 1] = f.read(VALUE_SIZE)
