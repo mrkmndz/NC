c1b8e20014e656462d051378a7fe31f4bec5cf11
Devon Warshaw <warshaw.devon@gmail.com>
2018-06-13 14:28:29 -0700

    artificial slowdown on server

--- server/server.py
+++ server/server.py
@@ -22,6 +22,8 @@ def counting():
         time.sleep(1)
 thread.start_new_thread(counting, ())
 
+speed = 1000
+
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.bind((SERVER_IP, NC_PORT))
 while True:
@@ -32,8 +34,10 @@ while True:
     
     if (nc_p.type == NC_READ_REQUEST or nc_p.type == NC_HOT_READ_REQUEST):
         rp_p = P4NetCache(type=NC_READ_REPLY, key=nc_p.key, value=lookup_val(nc_p.key))
+        time.sleep(1/speed)
         s.sendto(str(rp_p), (src_ip, NC_PORT))
         counter = counter + 1
     elif (nc_p.type == NC_UPDATE_REQUEST):
         rp_p = P4NetCache(type=NC_UPDATE_REPLY, key=nc_p.key, value=lookup_val(nc_p.key))
+        time.sleep(1/speed)
         s.sendto(str(rp_p), (CONTROLLER_IP, NC_PORT))
