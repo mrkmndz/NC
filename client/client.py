ed92547e6dc47fb755599afa9672e1c30385ff58
Devon Warshaw <warshaw.devon@gmail.com>
2018-06-13 14:11:49 -0700

    worker based model

--- client/client.py
+++ client/client.py
@@ -34,41 +34,54 @@ for i in range(1, num_query + 1):
         k = k + 1
     field[i] = k
 
-use_zipf = True
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.bind((CLIENT_IP, NC_PORT))
-last_print = time.time()
 responses = 0
+def worker(ip="10.0.0.1"):
+    global responses 
+    use_zipf = True
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.bind((ip, NC_PORT))
+    while (True):
+        if use_zipf:
+            r = random.randint(1, num_query)
+            key_header = field[r]
+        else:
+            key_header = random.randint(1, max_key)
+        key_field = struct.pack(">I", key_header)
+        for x in range(len_key - 4):
+            key_field += "\0"
+        rq_p = P4NetCache(type=NC_READ_REQUEST, key=key_field)
+        s.sendto(str(rq_p), (SERVER_IP, NC_PORT))
+        packet_str, src = s.recvfrom(1024)
+        nc_p = P4NetCache(packet_str)
+        if nc_p.type != NC_READ_REPLY:
+            print "unexpected response"
+            break
+        key_header = struct.unpack(">I", nc_p.key[:4])[0]
+        if key_header < 1 or key_header > 1000:
+            print "invalid key %d" % key_header
+            break
+        if nc_p.value != kv[key_header]:
+            print "data mismatch on key %d" % key_header
+            print "expected:"
+            print kv[key_header]
+            break
+        responses += 1
+    nc_p.show()
+
+from threading import Thread
+for x in range(3):
+    ip = "10.0.1.%d" % (x + 1)
+    print ip
+    t = Thread(target=worker, kwargs={"ip": ip})
+    t.setDaemon(True)
+    t.start()
+
+
+last_print = time.time()
 while (True):
-    if use_zipf:
-        r = random.randint(1, num_query)
-        key_header = field[r]
-    else:
-        key_header = random.randint(1, max_key)
-    key_field = struct.pack(">I", key_header)
-    for x in range(len_key - 4):
-        key_field += "\0"
-    rq_p = P4NetCache(type=NC_READ_REQUEST, key=key_field)
-    s.sendto(str(rq_p), (SERVER_IP, NC_PORT))
-    packet_str, src = s.recvfrom(1024)
-    nc_p = P4NetCache(packet_str)
-    if nc_p.type != NC_READ_REPLY:
-        print "unexpected response"
-        break
-    key_header = struct.unpack(">I", nc_p.key[:4])[0]
-    if key_header < 1 or key_header > 1000:
-        print "invalid key %d" % key_header
-        break
-    if nc_p.value != kv[key_header]:
-        print "data mismatch on key %d" % key_header
-        print "expected:"
-        print kv[key_header]
-        break
-    responses += 1
+    time.sleep(1)
     duration = time.time() - last_print
-    if duration > 1:
-        print "QPS = %f" % (responses / duration)
-        responses = 0
-        last_print = time.time()
-nc_p.show()
+    print "QPS = %f" % (responses / duration)
+    responses = 0
+    last_print = time.time()
 
--- server/server.py
+++ server/server.py
@@ -26,11 +26,13 @@ s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.bind((SERVER_IP, NC_PORT))
 while True:
     packet_str, src = s.recvfrom(2048)
+    src_ip, src_port = src
+    print src_ip
     nc_p = P4NetCache(packet_str)
     
     if (nc_p.type == NC_READ_REQUEST or nc_p.type == NC_HOT_READ_REQUEST):
         rp_p = P4NetCache(type=NC_READ_REPLY, key=nc_p.key, value=lookup_val(nc_p.key))
-        s.sendto(str(rp_p), (CLIENT_IP, NC_PORT))
+        s.sendto(str(rp_p), (src_ip, NC_PORT))
         counter = counter + 1
     elif (nc_p.type == NC_UPDATE_REQUEST):
         rp_p = P4NetCache(type=NC_UPDATE_REPLY, key=nc_p.key, value=lookup_val(nc_p.key))
